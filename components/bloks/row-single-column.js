9bf0c5d0a37b17b007ab9edcbb1fdaebc0f8cc76
Ovi Savescu <ovi@ovswebsites.com>
2022-02-04 11:44:31 +0200

    added vertical spacing to single column row

--- components/bloks/row-single-column.js
+++ components/bloks/row-single-column.js
@@ -3,23 +3,23 @@
 // *********************************
 // bloks: "content"
 
-import * as React from "react";
-import { blockIterator } from "../../utils/blockIterator";
+import * as React from "react"
+import { blockIterator } from "../../utils/blockIterator"
 
-import { SectionThemeContext } from "./section";
+import { SectionThemeContext } from "./section"
 
 const RowSingleColumn = ({ blok: row }) => {
   // console.log("two column row component", row)
 
-  const sectionTheme = React.useContext(SectionThemeContext);
-  const textStyles = sectionTheme.bgValue == "dark" ? "text-white" : "";
+  const sectionTheme = React.useContext(SectionThemeContext)
+  const textStyles = sectionTheme.bgValue == "dark" ? "text-white" : ""
 
   return (
-    <div className={textStyles}>
+    <div className={`${textStyles} space-y-20`}>
       {/* <h2>This is a two column row</h2> */}
       {blockIterator(row.content)}
     </div>
-  );
-};
+  )
+}
 
-export default RowSingleColumn;
+export default RowSingleColumn
