76341b7c3eab805840985e8046eee8d23129bda8
Ovi Savescu <ovi@ovswebsites.com>
2022-02-16 17:27:36 +0200

    removed old comment

--- components/Page.js
+++ components/Page.js
@@ -13,22 +13,6 @@ const Page = ({ blok }) => {
             <DynamicComponent blok={blok} key={blok._uid} />
           ))
         : null}
-
-      <div>
-        {/* <Icon name="TestIcon" /> */}
-        {/* <div style={{ margin: "0 auto", maxWidth: "600px" }}>
-          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-            <div className="text-red-500">
-              <IconSmort name="group" />
-              <p>Fire</p>
-            </div>
-            <div className="text-red-500">
-              <IconSmort name="chevronRight" />
-              <p>Fire</p>
-            </div>
-          </div>
-        </div> */}
-      </div>
     </main>
   )
 }
