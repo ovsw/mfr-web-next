af5185abaf1ac554c0f0540033fb7c1efcf8247a
Ovi Savescu <ovi@ovswebsites.com>
2022-02-17 18:09:38 +0200

    added non-working overlay for mobile menu

--- components/Header/index.js
+++ components/Header/index.js
@@ -42,186 +42,204 @@ const Header = ({ props }) => {
   return (
     <header>
       <Popover className="relative bg-white">
-        <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-4 md:py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
-          {/* LOGO */}
-          <div className="flex justify-start lg:w-0 lg:flex-1">
-            <Link href="/">
-              <a className="w-40 pt-2 md:pt-0 md:h-10 relative">
-                <span className="sr-only">Marianna's Fundraisers</span>
-                <Image
-                  className=""
-                  src={DesktopLogo}
-                  alt="Marianna's Fundraisers Logo"
-                  fit="fit"
+        {({ open }) => (
+          <>
+            {/* <Popover.Overlay
+              className={`${
+                open ? "opacity-30 fixed inset-0" : "opacity-0"
+              } bg-black`}
+            /> */}
+            <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-4 md:py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
+              {/* LOGO */}
+              <div className="flex justify-start lg:w-0 lg:flex-1">
+                <Link href="/">
+                  <a className="w-40 pt-2 md:pt-0 md:h-10 relative">
+                    <span className="sr-only">Marianna's Fundraisers</span>
+                    <Image
+                      className=""
+                      src={DesktopLogo}
+                      alt="Marianna's Fundraisers Logo"
+                      fit="fit"
+                    />
+                  </a>
+                </Link>
+              </div>
+              {/* LOGO END */}
+
+              {/* MOBILE TRIGGER */}
+              <div className="-mr-2 -my-2 md:hidden">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-stone-400 hover:text-stone-500 hover:bg-stone-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
+                  <span className="sr-only">Open menu</span>
+                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+              {/* END MOBILE TRIGGER */}
+
+              {/* DESKTOP NAV */}
+              <Popover.Group as="nav" className="hidden md:flex space-x-10">
+                <DesktopDropdown
+                  title="Fundraisers"
+                  items={fundraisers}
+                  description="We offer 14” famous hoagies and 12” delicious pizzas for your fundraising needs. You'll love how easy they are to sell and your customers will love the variety."
                 />
-              </a>
-            </Link>
-          </div>
-          {/* LOGO END */}
 
-          {/* MOBILE TRIGGER */}
-          <div className="-mr-2 -my-2 md:hidden">
-            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-stone-400 hover:text-stone-500 hover:bg-stone-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
-              <span className="sr-only">Open menu</span>
-              <MenuIcon className="h-6 w-6" aria-hidden="true" />
-            </Popover.Button>
-          </div>
-          {/* END MOBILE TRIGGER */}
+                <Link href="/menu">
+                  <a className="main-menu-item text-stone-800">Menu</a>
+                </Link>
 
-          {/* DESKTOP NAV */}
-          <Popover.Group as="nav" className="hidden md:flex space-x-10">
-            <DesktopDropdown
-              title="Fundraisers"
-              items={fundraisers}
-              description="We offer 14” famous hoagies and 12” delicious pizzas for your fundraising needs. You'll love how easy they are to sell and your customers will love the variety."
-            />
+                <DesktopDropdown
+                  title="About Us"
+                  items={aboutUs}
+                  description="We have over 40 years of experience helping groups like yours raise money. We guide you through every step to help you have the best fundraiser possible."
+                />
+                <DesktopDropdown
+                  title="Selling Tools"
+                  items={tools}
+                  description="We have a variety of tools to help make your fundraising experience a little easier."
+                />
 
-            <Link href="/menu">
-              <a className="main-menu-item text-stone-800">Menu</a>
-            </Link>
+                <Link href="/contact-us">
+                  <a className="main-menu-item text-stone-800">Contact Us</a>
+                </Link>
+              </Popover.Group>
 
-            <DesktopDropdown
-              title="About Us"
-              items={aboutUs}
-              description="We have over 40 years of experience helping groups like yours raise money. We guide you through every step to help you have the best fundraiser possible."
-            />
-            <DesktopDropdown
-              title="Selling Tools"
-              items={tools}
-              description="We have a variety of tools to help make your fundraising experience a little easier."
-            />
-
-            <Link href="/contact-us">
-              <a className="main-menu-item text-stone-800">Contact Us</a>
-            </Link>
-          </Popover.Group>
-
-          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            {/* <a
+              <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                {/* <a
               href="#"
               className="whitespace-nowrap text-base font-medium text-stone-500 hover:text-stone-900"
             >
               Sign in
             </a> */}
-            <Link href="/schedule-a-free-tasting">
-              <a
-                className="
+                <Link href="/schedule-a-free-tasting">
+                  <a
+                    className="
               ml-8 whitespace-nowrap 
               inline-flex items-center justify-center 
               btn btn-outlined-accent
               btn-small
               "
-              >
-                Book a Free Tasting
-              </a>
-            </Link>
-          </div>
-        </div>
-        {/* END DESKTOP NAV */}
-
-        {/* MOBILE MENU  */}
-        <Transition
-          as={Fragment}
-          enter="duration-200 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Popover.Panel
-            focus
-            className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-          >
-            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-              {/* MOBILE MENU MAIN */}
-              <div className="pt-5 pb-6 px-5">
-                {/* MOBILE MENU TOP */}
-                <div className="flex items-center justify-between">
-                  <Link href="">
-                    <a className="w-24">
-                      <Image
-                        src={DesktopLogo}
-                        alt="Marianna's Fundraisers Logo"
-                      />
-                    </a>
-                  </Link>
-
-                  <div className="-mr-2">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-stone-400 hover:text-stone-500 hover:bg-stone-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-accent">
-                      <span className="sr-only">Close menu</span>
-                      <XIcon className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
-                  </div>
-                </div>
-                {/*END MOBILE MENU TOP*/}
-
-                {/* fundraisers NAV ITEM SUBITEMS ONLY */}
-                <div className="mt-6">
-                  <nav className="grid grid-cols-1 gap-7">
-                    {mobileMainMenu.map(item => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="-m-3 p-3 flex items-center rounded-lg hover:bg-stone-50"
-                      >
-                        <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gradient-to-r from-red-600 to-accent text-white">
-                          <item.icon className="h-6 w-6" aria-hidden="true" />
-                        </div>
-                        <div className="ml-4 text-base font-medium text-stone-900">
-                          {item.name}
-                        </div>
-                      </a>
-                    ))}
-                  </nav>
-                </div>
-                {/* SOLUTIONS NAV ITEM SUBITEMS ONLY */}
-              </div>
-              {/* END MOBILE MENU MAIN */}
-
-              {/* MOBILE MENU EXTRA ITEMS */}
-              <div className="py-6 px-5">
-                {/* EXTRA ITEMS MAIN */}
-                <div className="grid grid-cols-2 gap-4">
-                  {mobileSecondaryMenu.map((item, i) => {
-                    return (
-                      <Link key={i} href={item.href}>
-                        <a className="text-base font-medium text-stone-900 hover:text-stone-700">
-                          {item.name}
-                        </a>
-                      </Link>
-                    )
-                  })}
-                </div>
-                {/* END EXTRA ITEMS MAIN */}
-
-                {/* EXTRA ITEMS BUTTONS  */}
-                <div className="mt-6">
-                  <Link href="/schedule-a-free-tasting">
-                    <a className="btn btn-accent btn-small">
-                      <span className="mr-2">
-                        <CalendarIcon className="h-5 w-5" aria-hidden="true" />
-                      </span>
-                      Book Free Tasting
-                    </a>
-                  </Link>
-                  <p className="mt-4 text-center text-base font-medium text-stone-500">
-                    {/* Existing customer? */}
-                    <Link href="/get-support">
-                      <a className="btn btn-outlined-primary btn-small">
-                        <span className="mr-2">
-                          <PhoneIcon className="h-5 w-5" aria-hidden="true" />
-                        </span>{" "}
-                        <span className="">Get Support / Contact</span>
-                      </a>
-                    </Link>
-                  </p>
-                </div>
-                {/* END EXTRA ITEMS BUTTONS  */}
+                  >
+                    Book a Free Tasting
+                  </a>
+                </Link>
               </div>
             </div>
-          </Popover.Panel>
-        </Transition>
+            {/* END DESKTOP NAV */}
+
+            {/* MOBILE MENU  */}
+            <Transition
+              as={Fragment}
+              enter="duration-200 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Popover.Panel
+                focus
+                className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+              >
+                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                  {/* MOBILE MENU MAIN */}
+                  <div className="pt-5 pb-6 px-5">
+                    {/* MOBILE MENU TOP */}
+                    <div className="flex items-center justify-between">
+                      <Link href="">
+                        <a className="w-24">
+                          <Image
+                            src={DesktopLogo}
+                            alt="Marianna's Fundraisers Logo"
+                          />
+                        </a>
+                      </Link>
+
+                      <div className="-mr-2">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-stone-400 hover:text-stone-500 hover:bg-stone-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-accent">
+                          <span className="sr-only">Close menu</span>
+                          <XIcon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                      </div>
+                    </div>
+                    {/*END MOBILE MENU TOP*/}
+
+                    {/* fundraisers NAV ITEM SUBITEMS ONLY */}
+                    <div className="mt-6">
+                      <nav className="grid grid-cols-1 gap-7">
+                        {mobileMainMenu.map(item => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="-m-3 p-3 flex items-center rounded-lg hover:bg-stone-50"
+                          >
+                            <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gradient-to-r from-red-600 to-accent text-white">
+                              <item.icon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div className="ml-4 text-base font-medium text-stone-900">
+                              {item.name}
+                            </div>
+                          </a>
+                        ))}
+                      </nav>
+                    </div>
+                    {/* SOLUTIONS NAV ITEM SUBITEMS ONLY */}
+                  </div>
+                  {/* END MOBILE MENU MAIN */}
+
+                  {/* MOBILE MENU EXTRA ITEMS */}
+                  <div className="py-6 px-5">
+                    {/* EXTRA ITEMS MAIN */}
+                    <div className="grid grid-cols-2 gap-4">
+                      {mobileSecondaryMenu.map((item, i) => {
+                        return (
+                          <Link key={i} href={item.href}>
+                            <a className="text-base font-medium text-stone-900 hover:text-stone-700">
+                              {item.name}
+                            </a>
+                          </Link>
+                        )
+                      })}
+                    </div>
+                    {/* END EXTRA ITEMS MAIN */}
+
+                    {/* EXTRA ITEMS BUTTONS  */}
+                    <div className="mt-6">
+                      <Link href="/schedule-a-free-tasting">
+                        <a className="btn btn-accent btn-small">
+                          <span className="mr-2">
+                            <CalendarIcon
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                            />
+                          </span>
+                          Book Free Tasting
+                        </a>
+                      </Link>
+                      <p className="mt-4 text-center text-base font-medium text-stone-500">
+                        {/* Existing customer? */}
+                        <Link href="/get-support">
+                          <a className="btn btn-outlined-primary btn-small">
+                            <span className="mr-2">
+                              <PhoneIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>{" "}
+                            <span className="">Get Support / Contact</span>
+                          </a>
+                        </Link>
+                      </p>
+                    </div>
+                    {/* END EXTRA ITEMS BUTTONS  */}
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
       </Popover>
     </header>
   )
